0ddb4f7e69e86a46aefb6b906585c55aa485461a
loreineb <loreine-a.bernal@gcash.com>
2023-06-08 12:49:47 +0800

    committing my scratch

---
+++ src/main/java/scratch.java
@@ -0,0 +1,28 @@
+//Junit is a library, wala siyang void main, pero once you put it in ur code, magagamit mo
+//        Most widely used unit testing library in Java
+//        Pwede gamitin to check if it's producing the actual results
+//        Implementation of unit testing
+//
+//        What is unit testing?
+//        A type of software testing
+//        To validate software code to perform as expected (key word: as expected)
+//        Coupled ang implementation with testing
+//        Can isolate code: you don't test the entire code: kunyari, method lang or class lang
+//        per component testing
+//        Integrated testing: test integrated components
+//        system testing: overall system
+//        acceptance: customers na magtetest
+//        Often times, the code for testing is longer than the code for implementation
+//
+//        Classpath
+//        By default, eto yung folder na kayang basahin agad code
+//        JAR file ng JUnit, ilagay sa Classpath
+//
+//        Maven is dependency-based, si Maven na bahala, no need to dowload stuff na
+//
+//        TDD: Test driven development
+//
+//        Java streams
+//
+//        Pag nag mvn clean install ka and may nag fail na test, di siya tutuloy sa build
+//        May confidence ka ganun
